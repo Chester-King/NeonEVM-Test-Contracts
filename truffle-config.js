625c8dcfebe3c1fc5f31ec1041a1aa544df5a129
Madhur Dixit <dmadhur6@gmail.com>
2021-11-05 19:09:26 +0530

    README added

--- truffle-config.js
+++ truffle-config.js
@@ -18,16 +18,8 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 const HDWalletProvider = require('@truffle/hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
 const mnemonic = require("./secrets.json").mnemonic;
 
 module.exports = {
